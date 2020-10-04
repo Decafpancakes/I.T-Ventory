7d07c01bf7e6e3346a90cb82ecfda9446c6ce994
Reece Bourgeois <reecebourgeois@gmail/com>
2020-10-03 21:58:55 -0500

    Updated line graph

--- client/src/components/SimpleLineChart.js
+++ client/src/components/SimpleLineChart.js
@@ -7,36 +7,52 @@ import YAxis from "recharts/lib/cartesian/YAxis";
 import CartesianGrid from "recharts/lib/cartesian/CartesianGrid";
 import Tooltip from "recharts/lib/component/Tooltip";
 import Legend from "recharts/lib/component/Legend";
+import Axios from 'axios';
 
-const data = [
-  { name: "Mon", Orders: 2 },
-  { name: "Tue", Orders: 8 },
-  { name: "Wed", Orders: 15 },
-  { name: "Thu", Orders: 9 },
-  { name: "Fri", Orders: 4 },
-  { name: "Sat", Orders: 6 },
-  { name: "Sun", Orders: 10 }
-];
+export default class SimpleLineChart extends React.Component {
 
-function SimpleLineChart() {
-  return (
-    //Chart will change the Y scale based on the data given.
-    <ResponsiveContainer width="99%" height={320}>
-      <LineChart data={data}>
-        <XAxis dataKey="name" />
-        <YAxis />
-        <CartesianGrid vertical={false} strokeDasharray="3 3" />
-        <Tooltip />
-        <Legend />
-        <Line
-          type="monotone"
-          dataKey="Orders"
-          stroke="#8884d8"
-          activeDot={{ r: 8 }}
-        />
-      </LineChart>
-    </ResponsiveContainer>
-  );
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      client_orders: []
+    };
+  }
+
+  async componentDidMount() {
+    let data = await Axios.get('/api/dashboard_page/getLineChartData');
+    let array = [];
+    data.data.forEach(document => {
+      array.push({
+        name: document.client_name,
+        Orders: document.orders
+      });
+    });
+    this.setState(state => {
+      return {
+        client_orders: array
+      };
+    });
+  }
+
+  render(){
+    return(
+      //Chart will change the Y scale based on the data given.
+      <ResponsiveContainer width="99%" height={320}>
+        <LineChart data={this.state.client_orders}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <CartesianGrid vertical={false} strokeDasharray="3 3" />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="Orders"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    );
+  }
 }
-
-export default SimpleLineChart;
